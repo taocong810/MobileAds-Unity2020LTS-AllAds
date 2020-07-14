d0810f2df911d81aac7c90a9497a4943d8e5a328
gley <gley.mobi@gmail.com>
2020-07-14 11:26:19 +0300

    Admob added: v.5.2.0

---
+++ Assets/GoogleMobileAds/Editor/GleyAdmobPatch.cs
@@ -0,0 +1,14 @@
+﻿namespace GoogleMobileAds.Editor
+{
+	public class GleyAdmobPatch 
+	{
+        public static void SetAdmobAppID(string androidAppId, string iosAppID)
+        {
+#if USE_ADMOB
+            GoogleMobileAdsSettings.Instance.IsAdMobEnabled = true;
+            GoogleMobileAdsSettings.Instance.AdMobAndroidAppId = androidAppId;
+            GoogleMobileAdsSettings.Instance.AdMobIOSAppId = iosAppID;
+#endif
+        }
+    }
+}
