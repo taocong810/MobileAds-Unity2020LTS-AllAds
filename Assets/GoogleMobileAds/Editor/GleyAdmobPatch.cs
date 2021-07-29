379b56d3bcac145d624d79309fb929e1d19471a8
Gley <gley.mobi@gmail.com>
2021-07-29 17:19:24 +0300

    Admob v6.0.2 added

---
+++ Assets/GoogleMobileAds/Editor/GleyAdmobPatch.cs
@@ -0,0 +1,15 @@
+﻿namespace GoogleMobileAds.Editor
+{
+    public class GleyAdmobPatch
+    {
+        public static void SetAdmobAppID(string androidAppId, string iosAppID)
+        {
+#if USE_ADMOB
+            GoogleMobileAdsSettings.Instance.DelayAppMeasurementInit = true;
+            GoogleMobileAdsSettings.Instance.GoogleMobileAdsAndroidAppId = androidAppId;
+            GoogleMobileAdsSettings.Instance.GoogleMobileAdsIOSAppId = iosAppID;
+            GoogleMobileAdsSettings.Instance.WriteSettingsToFile();
+#endif
+        }
+    }
+}
